dc475759a08aa4d20636ef53afa8ad0e19247976
lcdungo <lcdungo@up.edu.ph>
2021-04-08 17:41:09 +0800

    updated db and connection

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 DEFINE ('DB_USER', 'root');
 DEFINE ('DB_PASSWORD', '');
 DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'dmcs_survey_db');
+DEFINE ('DB_NAME', 'dmcs-ae-survey-db');
 
 $db_conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 	OR die ("Could not connect to the database because: ". mysqli_connect_error());
---
+++ dmcs-ae-survey-db.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 08, 2021 at 11:38 AM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dmcs-ae-survey-db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alum_survey`
+--
+
+CREATE TABLE `alum_survey` (
+  `answer_id` int(11) NOT NULL,
+  `question_num` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `answer_body` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emp_survey`
+--
+
+CREATE TABLE `emp_survey` (
+  `answer_id` int(11) NOT NULL,
+  `question_num` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `answer_body` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `token` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alum_survey`
+--
+ALTER TABLE `alum_survey`
+  ADD PRIMARY KEY (`answer_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `emp_survey`
+--
+ALTER TABLE `emp_survey`
+  ADD PRIMARY KEY (`answer_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alum_survey`
+--
+ALTER TABLE `alum_survey`
+  MODIFY `answer_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `emp_survey`
+--
+ALTER TABLE `emp_survey`
+  MODIFY `answer_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `alum_survey`
+--
+ALTER TABLE `alum_survey`
+  ADD CONSTRAINT `alum_survey_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
