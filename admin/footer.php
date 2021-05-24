77c86737e62979c6ca6ba0cb9ce7210f7e7c0743
lcdungo <lcdungo@up.edu.ph>
2021-05-25 07:29:32 +0800

    added nars' and cari's files
    - thank you page
    - updated emp_survey with fixed alignment and new values
    - footer for admin

--- admin/footer.php
+++ admin/footer.php
@@ -1,8 +1,12 @@
 <footer>
-        <p class="info">Department of Mathematics and Computer Science <br>
-        College of Science <br> 
-        University of the Philippines Baguio</p>
-        <p class="icons">In case of technical problems, you may contact us through:
-        <a href="https://www.facebook.com/upbdmcs/" target="_blank" title="Department of Mathematics and Computer Science, University of the Philippines Baguio"><i class="fa fa-facebook"></i></a>
-        <a href="#" target="_blank" title="Email"><i class="fa fa-at"></i></a></p>
-    </footer>
+    <p class="info">Department of Mathematics and Computer Science <br>
+		College of Science <br> 
+		University of the Philippines Baguio
+	</p>
+	<p class = "copyright"> &#169; 2021 | 
+							<a href="mailto:ncevangelista2@up.edu.ph"> Evangelista </a> | 
+							<a href="mailto:rmcalbario@up.edu.ph"> Calbario </a> | 
+							<a href="mailto:lcdungo@up.edu.ph"> Du&#241;go </a> | 
+							<a href="mailto:fcurdas@up.edu.ph"> Urdas </a>
+	</p>
+</footer>
--- thankyou.php
+++ thankyou.php
@@ -14,6 +14,17 @@
 	<script src="js/jquery.js"></script>
 	<script type="text/javascript" src="js/survey_fcn.js"></script>
 	<title>Thank You!</title>
+	<style>
+	img {
+  		display: block;
+  		margin-left: auto;
+  		margin-right: auto;
+		}
+
+	div.text {
+  text-align: center;
+	}
+	</style>
 	</head>
 		<!-- for header/ navbar    -->
 	<?php include('header.php'); ?>
@@ -24,7 +35,18 @@
 	<br>
 	<br>
 	<br>
-	<p> Thank you for your feedback! We greatly appreciate your time. </p>
+	<br>
+
+    <img src="images/check.png" alt="check mark" style="width:100px;height: 100px;px" />
+
+	<br>
+	<br>
+
+	<div class="text">
+		<h2>Thank you for your feedback!</h2>
+		<h3> We greatly appreciate your time. </h3>
+	</div>
+	
 	
 	<!-- for footer/ contact details -->
 		<?php include('footer.php'); ?>
