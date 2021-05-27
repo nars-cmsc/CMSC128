01026e9cee16d9db7021e1be9ecd3e2852aec895
Cari-Cari <71480323+Cari-Cari@users.noreply.github.com>
2021-05-27 11:13:27 +0800

    Will delete log in folder (Other Webpages)
    -  nilabas lang yung files from the log in folder

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer>
+	<p class="info">Department of Mathematics and Computer Science <br>
+		College of Science <br> 
+		University of the Philippines Baguio
+	</p>
+	<p class="icons">Contact us:
+		<a href="https://www.facebook.com/upbdmcs/" target="_blank" title="Department of Mathematics and
+			Computer Science, University of the Philippines Baguio"><i class="fa fa-facebook"></i> 
+				DMCS UPB Facebook page </a>
+		<a href="mailto:dmcs.upbaguio@up.edu.ph" target="_blank" title="dmcs.upbaguio@up.edu.ph"><i class="fa fa-at"></i> 
+				dmcs.upbaguio@up.edu.ph</a>
+	</p><br><br><br><br>
+	<p class = "copyright"> &#169; 2021 | 
+							<a href="mailto:ncevangelista2@up.edu.ph"> Evangelista </a> | 
+							<a href="mailto:rmcalbario@up.edu.ph"> Calbario </a> | 
+							<a href="mailto:lcdungo@up.edu.ph"> Du&#241;go </a> | 
+							<a href="mailto:fcurdas@up.edu.ph"> Urdas </a> </p>
+</footer>
---
+++ thankyou.php
@@ -0,0 +1,54 @@
+
+
+<!--Thank you! -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="icon" type="images/png" href="images/UP_seal.png">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/survey_fcn.js"></script>
+	<title>Thank You!</title>
+	<style>
+	img {
+  		display: block;
+  		margin-left: auto;
+  		margin-right: auto;
+		}
+
+	div.text {
+  text-align: center;
+	}
+	</style>
+	</head>
+		<!-- for header/ navbar    -->
+	<?php include('header.php'); ?>
+	<!-- label class = option; radio bttn class = options -->
+	
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+
+    <img src="images/check.png" alt="check mark" style="width:100px;height: 100px;px" />
+
+	<br>
+	<br>
+
+	<div class="text">
+		<h2>Thank you for your feedback!</h2>
+		<h3> We greatly appreciate your time. </h3>
+	</div>
+	
+	
+	<!-- for footer/ contact details -->
+		<?php include('footer.php'); ?>
+	
+</html>
