43b62de77a62ac11b691997d4ba2f6e3da965ee0
lcdungo <lcdungo@up.edu.ph>
2021-06-16 23:46:07 +0800

    minor fixes to missed items
    - edited some items i forgot

--- admin/alum_charts.php
+++ admin/alum_charts.php
@@ -402,13 +402,18 @@ if ($_SESSION['role'] == 3) {
                   datalabels: {
                   formatter: (value, ctx) => {
                   
-                    let sum = 0;
-                    let dataArr = ctx.chart.data.datasets[0].data;
-                    dataArr.map(data => {
-                        sum += data;
-                    });
-                    let percentage = (value*100 / sum).toFixed(2)+"%";
-                    return percentage;
+                    if (value > 0) {
+                       let sum = 0;
+                        let dataArr = ctx.chart.data.datasets[0].data;
+                        dataArr.map(data => {
+                            sum += data;
+                        });
+                        let percentage = (value*100 / sum).toFixed(2)+"%";
+                        return percentage; 
+                    } else {
+                        value = "";
+                        return value;
+                    }
 
                 
                   },
@@ -777,13 +782,18 @@ if ($_SESSION['role'] == 3) {
                 datalabels: {
                   formatter: (value, ctx) => {
                   
-                    let sum = 0;
-                    let dataArr = ctx.chart.data.datasets[0].data;
-                    dataArr.map(data => {
-                        sum += data;
-                    });
-                    let percentage = (value*100 / sum).toFixed(2)+"%";
-                    return percentage;
+                    if (value > 0) {
+                       let sum = 0;
+                        let dataArr = ctx.chart.data.datasets[0].data;
+                        dataArr.map(data => {
+                            sum += data;
+                        });
+                        let percentage = (value*100 / sum).toFixed(2)+"%";
+                        return percentage; 
+                    } else {
+                        value = "";
+                        return value;
+                    }
 
                 
                   },
