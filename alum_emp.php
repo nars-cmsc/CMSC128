e8e2edf13b971969956f58926c685c3285a5bb62
lcdungo <lcdungo@up.edu.ph>
2021-06-21 23:01:14 +0800

    added disabled feature to answered survey

--- alum_emp.php
+++ alum_emp.php
@@ -1,6 +1,19 @@
 <?php //require_once 'controllers/authentication.php'; 
 
 session_start();
+require ('config/connection.php');
+
+$id = $_SESSION['id'];
+// if user already answered employer survey
+$emp_check_query = "SELECT * FROM emp_survey_q1 WHERE user_id='$id' LIMIT 1";
+$result1 = mysqli_query($db_conn, $emp_check_query);
+$user_emp = mysqli_fetch_assoc($result1);
+
+// if user already answered alumni survey
+$alum_check_query = "SELECT * FROM alum_survey_q1 WHERE user_id='$id' LIMIT 1";
+$result2 = mysqli_query($db_conn, $alum_check_query);
+$user_alum = mysqli_fetch_assoc($result2);
+
 // if user is not logged in
 if (!isset($_SESSION['email'])) {
 	header('location:login.php');
@@ -36,11 +49,25 @@ if ($_SESSION['role'] == 2) {
 
 	<div class="container-info-ae">
 		<h3>
-			Which survey would you like to answer first?
+			Which survey would you like to answer?
 		</h3>
+		<h6 style="color:black;">
+			(If the button is disabled, it means you have answered that survey)
+		</h6>
 		<div class = "ae-buttons">
-				<a href="alum_survey.php" class="ae-choices" name="alumni" title="Alumni Survey">Alumni</a>
-				<a href="emp_survey.php" class="ae-choices" name="employer" title="Employer Satisfaction Survey">Employer</a>
+				<a href="alum_survey.php" class="ae-choices" name="alumni" title="Alumni Survey" 
+				<?php if($user_alum && $_SESSION['role']==3){ if ($user_alum['user_id'] == $_SESSION['id']) {?>
+					onclick="return false;" style="background-color:gray;"
+		   		<?php } else ?> 
+		   			onclick="return true;"
+		   		<?php }?>>Alumni</a>
+
+				<a href="emp_survey.php" class="ae-choices" name="employer" title="Employer Satisfaction Survey" 
+				<?php if($user_emp && $_SESSION['role']==3){ if ($user_emp['user_id'] == $_SESSION['id']) {?>
+					onclick="return false;" style="background-color:gray;"
+		   		<?php } else ?> 
+		   			onclick="return true;"
+		   		<?php }?>>Employer</a>
 		</div>
 	</div>
 	<?php include('footer.php'); ?>
