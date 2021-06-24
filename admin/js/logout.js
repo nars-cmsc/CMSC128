0b754540c75ac67585fb27ce77b56eb689423d25
Cari-Cari <71480323+Cari-Cari@users.noreply.github.com>
2021-06-24 22:35:00 +0800

    Removed Function for " x "
    - removed function for " x " button

--- admin/js/logout.js
+++ admin/js/logout.js
@@ -1,12 +1,6 @@
 var modal = document.getElementById('logoutModal');
-var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-closeBtn.addEventListener('click', closeModal);
 window.addEventListener('click', clickOutside);
-
-function closeModal(){
-	modal.style.display = 'none';
-}
 
 function clickOutside(e){
 	if(e.target == modal){
